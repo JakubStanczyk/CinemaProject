53937424f1db95ff73125971db67f9c4896778a2
JakubStanczyk <stanczykjakub97@gmail.com>
2020-12-02 17:00:26 +0000

    movie class half filled

--- src/Movie.java
+++ src/Movie.java
@@ -1,4 +1,30 @@
 
 public class Movie {
+	
+	int movieId;
+	String movieName;
+	String movieGenre;
+	
+	public Movie() {
+		
+	}
+	
+	public Movie(int movieID, String movieName, String movieGenre)
+	{
+		this.movieId = movieID;
+		this.movieName = movieName;
+		this.movieGenre = movieGenre;
+		
+	}
+	
+	public int getMovieId() {
+		return movieId;
+		
+	}
+	
+	public String getMovieName()
+		return getMovieName;
 
 }
+
+	public String
