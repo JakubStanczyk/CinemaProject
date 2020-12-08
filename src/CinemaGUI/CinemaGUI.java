2fdd27e1415ab0ac6079d51554705caad40bfa3d
Robert Womack <35308111+catbot98@users.noreply.github.com>
2020-12-08 19:55:40 +0000

    preliminary login page
    added text fields and labels plus some test functionality to button

--- src/CinemaGUI/CinemaGUI.java
+++ src/CinemaGUI/CinemaGUI.java
@@ -1,11 +1,13 @@
 package CinemaGUI;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.TilePane;
 
 public class CinemaGUI extends Application {
 	public static void main(String args[]) {
@@ -14,17 +16,42 @@ public class CinemaGUI extends Application {
 
 	@Override
 	public void start(Stage stage) {
-		stage.setTitle("Cinema Booking System");
+		// set title
+		stage.setTitle("Login");
+		
+		// set layout, padding, and orientation
+		TilePane tilePane = new TilePane();
+		tilePane.setHgap(10);
+		tilePane.setOrientation(Orientation.VERTICAL);
+
+		// create scene and add layout
+		Scene scene = new Scene(tilePane, 350, 250);
+		
+		// make labels and textfields
+		Label userLbl = new Label("username");
+		TextField userTxtF = new TextField();
+		Label passwordLbl = new Label("password");
+		TextField passwordTxtF = new TextField();
+		
+		// make login button and set action
 		Button loginBtn = new Button("Login");
 		loginBtn.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
+			// button returns value of userTxtF when pressed
 			public void handle(ActionEvent event) {
-				System.out.print("Button works");
+				System.out.print(userTxtF.getText());
 			}
 		});
-		StackPane root = new StackPane();
-		root.getChildren().add(loginBtn);
-		stage.setScene(new Scene(root, 350, 250));
+		
+		// add elements to layout
+		tilePane.getChildren().add(userLbl);
+		tilePane.getChildren().add(userTxtF);
+		tilePane.getChildren().add(passwordLbl);
+		tilePane.getChildren().add(passwordTxtF);
+		tilePane.getChildren().add(loginBtn);
+		
+		// set and show scenes
+		stage.setScene(scene);
 		stage.show();
 	}
 }
