33d315867113197a9ce6b27a85827e9094a69c4c
Robert Womack <35308111+catbot98@users.noreply.github.com>
2020-12-10 00:42:16 +0000

    created generic alet window
    can pass in title of window and alert message

---
+++ src/CinemaGUI/AlertWindow.java
@@ -0,0 +1,28 @@
+package CinemaGUI;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class AlertWindow {
+	public static void display(String title, String message) {
+		Stage window = new Stage();
+		window.setTitle(title);
+		window.initModality(Modality.APPLICATION_MODAL);
+		
+		VBox layout = new VBox(10);
+		layout.setPadding(new Insets(10,10,10,10));
+		layout.setAlignment(Pos.CENTER);
+		
+		Label messageLbl = new Label(message);
+		layout.getChildren().addAll(messageLbl);
+		
+		Scene alertScene = new Scene(layout, 300, 200);
+		window.setScene(alertScene);
+		window.showAndWait();
+	}
+}
