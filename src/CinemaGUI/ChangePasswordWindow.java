754f94b0c132e4420f14ac64cb1ffd5f3c6e1101
Robert Womack <35308111+catbot98@users.noreply.github.com>
2020-12-10 01:02:28 +0000

    create change password window

---
+++ src/CinemaGUI/ChangePasswordWindow.java
@@ -0,0 +1,42 @@
+package CinemaGUI;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class ChangePasswordWindow {
+	public static void display() {
+		Stage window = new Stage();
+		window.setTitle("Delete Account");
+		
+		window.initModality(Modality.APPLICATION_MODAL);
+		window.setMaxWidth(300);
+		
+		Label newPassLabl = new Label("New Password");
+		PasswordField newPasswordF = new PasswordField();
+		Label confirmPassLbl = new Label("Confirm Password");
+		PasswordField confirmPasswordF = new PasswordField();
+		
+		Button changePassBtn = new Button("Change Password");
+		changePassBtn.setOnAction(e -> {
+			if(newPasswordF.getText().equals(confirmPasswordF.getText())) {
+				System.out.println("passwords match\nTODO implement password change");
+			} else {
+				AlertWindow.display("Error", "Passwords don't match");
+			}
+		});
+		
+		VBox layout = new VBox(10);
+		layout.setPadding(new Insets(10,10,10,10));
+		layout.setAlignment(Pos.CENTER);
+		layout.getChildren().addAll(newPassLabl, newPasswordF,
+				confirmPassLbl, confirmPasswordF, changePassBtn);
+		Scene confirmScene = new Scene(layout, 300, 200);
+		window.setScene(confirmScene);
+		window.showAndWait();
+	}
+}
